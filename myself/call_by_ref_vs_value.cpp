d95fabea448f7d23876bead2059d429987e1f9bd
HanseamChung <35828006+HanseamChung@users.noreply.github.com>
2018-11-10 18:13:25 +0100

    Add files via upload

--- myself/call_by_ref_vs_value.cpp
+++ myself/call_by_ref_vs_value.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
+/*
 //call by value
 //call by-reference
 
@@ -27,7 +28,41 @@ int main()
     swap(a,b); // call by-value
     printf("a = %d, b = %d\n",a,b);
 
-    swap2(a,b); // call by-reference;
+    swap2(a,b); // call by-reference
     printf("a = %d, b = %d\n",a,b);
 
 }
+*/
+
+
+
+//prototype declare first and define later
+
+void walk(int );
+void rotate(int );
+void drawSquare();
+
+int main()
+{
+    drawSquare();
+}
+
+void walk(int distance)
+{
+ printf("walked %dcm\n",distance);
+}
+
+void rotate(int angle)
+{
+    printf("rotated %d degree", angle);
+}
+
+void drawSquare()
+{
+    for(int i = 1 ; i <=4 ; i++)
+    {
+        walk(10);
+        rotate(90);
+    }
+}
+
